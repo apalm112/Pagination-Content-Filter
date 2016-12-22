4c4ca7bad4361abfc91e883d134729e019a2e31c
apalm112 <apalm112@gmail.com>
2016-12-21 22:23:51 -0800

    Started functions to create & append correct number of pagination links.

--- js/pagination.js
+++ js/pagination.js
@@ -1,49 +1,57 @@
 //Problem: Page currently has no pagination, so all students are listed on page
 //Solution: Implement a pagination feature w/ unobtrusive JS to show only 10 students at a time, w/ clickable links appended that will list further students
 
-// <ul class="student-list">
-//   <li class="student-item cf">
-//       <div class="student-details">
-//           <img class="avatar" src="https://randomuser.me/api/portraits/thumb/women/67.jpg">
-//           <h3>iboya vat</h3>
-//           <span class="email">iboya.vat@example.com</span>
-//       </div>
-//   </li>
-
 //IF browser has JS disabled, Then index.html should display entire list of students
 
-/* Global Variables -------*/
+/* Global Variables ------------------------------------*/
 var $studentList = $('.student-list');  //to hide student ul element
 var $student = $('.student-item');
 
 var studentArray = []; //Global Array to hold students found on web page
 var groupsOfTenStudents = []; //Global Array to hold smaller arrays of groups of ten students
-var devArray = []; //for the whole li object
 
+/* Main Functions ----------------------------------------*/
+//Script must work for any number of students
 
-// When page loads:  the script will traverse throught the DOM
-//(by selecting '.student-details' then getting its child that's a <h3>)
-function getStudents() {
-    //Iterate thru DOM to collect all the students
-    var getStudent = document.getElementsByClassName('student-item');
-    // for (var idx=0; idx < getStudent.length; idx++){
-    //     //console.log(getStudent[idx].childNodes[3].innerHTML);
-    //     studentArray.push(getStudent[idx].childNodes[3].innerHTML);
-    // }
-    for (var idx=0;idx<getStudent.length;idx++) {
-        devArray.push(getStudent[idx].childNodes);
-    }
+function showFirstTenStudents() {
+    $student.slice(0, 10).css('display', 'list-item');
+    //this line almost worked, just need to add li border
+    //$stuList.prepend(groupsOfTenStudents[0].slice(0, 10));
+    displayPaginationLinks();
 }
 
-//Script must work for any number of students
+function displayPaginationLinks() {
+    calculatePagesNeeded();
+    //All functionality to be appended dynamically to the DOM
+
+    $studentList.append(createPageLinks);
+}
+
 function calculatePagesNeeded() {
-    // calculate the number of pages needed & pagination links to display
-    var numberOfStudents = devArray.length;
+    // calculate the number of pages & links needed based on 10 students/page
+    var numberOfStudents = $student.length;
     var numberOfPaginationLinks = Math.ceil(numberOfStudents / 10);
     return numberOfPaginationLinks;
 }
 
-function getFirstTenStudents() {
+function createPageLinks() {
+    var numberOfPageLinks = calculatePagesNeeded();
+    var paginationLink = '<div class="pagination">';
+    paginationLink += '<ul><li>';
+    paginationLink += '<a class="active" href="#">' + 1 + '</a>';
+    paginationLink += '</li>';
+    //repeat
+    paginationLink += '</ul></div>';
+    return paginationLink;
+}
+
+
+function paginationClicked() {
+    //when pagination links clicked on, they show appropriate list of students
+
+}
+
+function getTenStudents() {
     //group devArray into arrays of ten students each
     for (var idx=0; idx < devArray.length; idx += 10) {
         groupsOfTenStudents.push(devArray.slice(idx, (idx + 10)));
@@ -54,37 +62,8 @@ function getFirstTenStudents() {
     });
 }
 
-function showFirstTenStudents() {
-    $student.slice(0, 10).css('display', 'list-item');
-    //this line almost worked, just need to add li border
-    //$stuList.prepend(groupsOfTenStudents[0].slice(0, 10));
-}
 
 
 $(document).ready(function() {
-    //get the ul element & hide it
-    // $student.hide(); moved to CSS display: none;
     showFirstTenStudents();
-
 });
-
-
-function displayPaginationLinks() {
-    calculatePagesNeeded();
-    // display pagination links
-    //All functionality to be appended dynamically to the DOM
-    // <div class="pagination">
-    // <ul>
-    // <li>
-    // <a class="active" href="#">1</a>
-    // </li>
-    // //repeat
-    // </ul>
-    // </div>
-
-}
-
-function paginationClicked() {
-    //when pagination links clicked on, they show appropriate list of students
-
-}
