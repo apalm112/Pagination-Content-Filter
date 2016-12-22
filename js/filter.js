393babf3a9d2aebea9b65caade054cab367fb7e3
apalm112 <apalm112@gmail.com>
2016-12-22 01:28:06 -0800

    Started coding searchNames function to find matching student name.

--- js/filter.js
+++ js/filter.js
@@ -1,29 +1,14 @@
 //Problem: There is no input search feature to quickly find a student
 //Solution: Add JS to dynamically add a search input field to find all matching students
 
-/* Global Variable --------------------------------*/
+/* Global Variables --------------------------------*/
 var $student = $('.student-details');
-var studentArray = [];
 
-//Search button click returns all results that match name or emails which include matching name
+/* Main Functions ----------------------------------*/
 
-
-//This could work for specific name search
-//populates the studentArray w/ names only
-//$student[0][0][3].innerHTML;
-// var getStudent = document.getElementsByClassName('student-details');
-//Iterate thru DOM to collect all the students
-for (var idx=0; idx < $student.length; idx++){
-    //console.log($student[idx].childNodes[3].innerHTML);
-    studentArray.push($student[idx].childNodes[3].innerHTML);
-}
-
-
-
-//Include search input to look for particular student
-//Add it dynamically to index.html using the format:
 function appendSearchDiv() {
-    //
+    //Include search input to look for particular student
+    //Add it dynamically to index.html using the format:
     var search =  '<div class="student-search">';
     search += '<input placeholder="search for...">';
     search += '<button>Search</button>';
@@ -31,11 +16,41 @@ function appendSearchDiv() {
     $('.page-header').append(search);
 }
 
+//Search button click returns all results that match name or emails which include matching name
+function searchButtonClick() {
+    //get text value from input
 
+    //function call to search studentNames & studentEmails for matching name
+}
+
+function searchNames(){
+    var $input = $('.student-search');
+    var inputValue = $input[0].firstChild.value;
+
+    //Iterate thru DOM to collect all the students
+    for (var idx=0; idx < $student.length; idx++){
+        //console.log($student[idx].childNodes[1].innerText);
+        if ($student[idx].childNodes[1].innerText contains inputValue) {
+            console.log(inputValue);
+        }
+    }
+
+}
+
+function searchEmails() {
+    var studentEmails = [];
+
+}
+
+
+
+
+function noMatchesMessage() {
     //If no matches, then a message in the HTML tells user there are no matches
-
+}
 
 $(document).ready(function(){
     //function to append div, input, button to DOM
     appendSearchDiv();
+
 });
