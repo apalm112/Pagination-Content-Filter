a268247d9e5f0947a326e50b2860742987cf3627
apalm112 <apalm112@gmail.com>
2016-12-22 00:28:07 -0800

    Added function to build and append search input & button dynamically to the DOM.

--- js/filter.js
+++ js/filter.js
@@ -1,25 +1,41 @@
 //Problem: There is no input search feature to quickly find a student
 //Solution: Add JS to dynamically add a search input field to find all matching students
 
-//Include search input to look for particular student
-//Add it dynamically to index.html using the format:
-// <div class="student-search">
-//     <input placeholder="search for...">
-//     <button>Search</button>
-// </div>
-
+/* Global Variable --------------------------------*/
 var $student = $('.student-details');
 var studentArray = [];
+
 //Search button click returns all results that match name or emails which include matching name
 
-//If no matches, then a message in the HTML rells user there are no matches
-//Iterate thru DOM to collect all the students
 
 //This could work for specific name search
 //populates the studentArray w/ names only
 //$student[0][0][3].innerHTML;
 // var getStudent = document.getElementsByClassName('student-details');
+//Iterate thru DOM to collect all the students
 for (var idx=0; idx < $student.length; idx++){
     //console.log($student[idx].childNodes[3].innerHTML);
     studentArray.push($student[idx].childNodes[3].innerHTML);
 }
+
+
+
+//Include search input to look for particular student
+//Add it dynamically to index.html using the format:
+function appendSearchDiv() {
+    //
+    var search =  '<div class="student-search">';
+    search += '<input placeholder="search for...">';
+    search += '<button>Search</button>';
+    search += '</div>';
+    $('.page-header').append(search);
+}
+
+
+    //If no matches, then a message in the HTML tells user there are no matches
+
+
+$(document).ready(function(){
+    //function to append div, input, button to DOM
+    appendSearchDiv();
+});
