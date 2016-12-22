8a0b2d78896e0fabb0319b399ba527bea7acc8d0
apalm112 <apalm112@gmail.com>
2016-12-21 23:31:31 -0800

    Function to show set of 10 students to corresponding pagination link works.

--- js/pagination.js
+++ js/pagination.js
@@ -8,7 +8,6 @@ var $studentList = $('.student-list');  //to hide student ul element
 var $student = $('.student-item');
 
 var studentArray = []; //Global Array to hold students found on web page
-var groupsOfTenStudents = []; //Global Array to hold smaller arrays of groups of ten students
 
 /* Main Functions ----------------------------------------*/
 //Script must work for any number of students
@@ -16,13 +15,11 @@ var groupsOfTenStudents = []; //Global Array to hold smaller arrays of groups of
 function showFirstTenStudents() {
     $student.slice(0, 10).css('display', 'list-item');
     //this line almost worked, just need to add li border
-    //$stuList.prepend(groupsOfTenStudents[0].slice(0, 10));
+    //$stuList.prepend(studentArray[0].slice(0, 10));
     displayPaginationLinks();
 }
 
 function displayPaginationLinks() {
-    //All functionality to be appended dynamically to the DOM
-
     $studentList.append(createPageLinks);
 }
 
@@ -39,33 +36,39 @@ function createPageLinks() {
     var counter = 0;
     for (var idx=0;idx<numberOfPageLinks;idx++) {
         counter ++;
-        paginationLink += '<li>';
-        paginationLink += '<a class="active" href="#potato">' + counter + '</a>';
-        paginationLink += '</li>';
+        paginationLink += '<li><a class="active" href="#' + counter + '">';
+        paginationLink += counter + '</a></li>';
     }
     paginationLink += '</ul></div>';
     return paginationLink;
 }
 
-
 function paginationClicked() {
     //when pagination links clicked on, they show appropriate list of students
+    $('.active').on('click', function() {
+        //call function that gets corresponding <a> set of students to display
+        console.log(this.innerHTML);
+        var number = this.innerHTML;
+        var highNumber = number * 10;
+        var lowNumber = highNumber - 10;
+        // if else conditional to get previous set of students diplay:none;
+        $student.slice(lowNumber, highNumber).css('display', 'list-item');
 
-}
-
-function getTenStudents() {
-    //group devArray into arrays of ten students each
-    for (var idx=0; idx < devArray.length; idx += 10) {
-        groupsOfTenStudents.push(devArray.slice(idx, (idx + 10)));
-        $student.remove([idx]);
-    }
-    $student.load(function() {
-        $(this).css('display', 'list-item');
     });
 }
 
+function getTenStudents() {
+    //group array into arrays of ten students each
+    // for (var idx=0; idx < $student.length; idx++) {
+    //     studentArray.push($student[idx]);
+    // }
+
+
+}
 
 
 $(document).ready(function() {
     showFirstTenStudents();
+    getTenStudents();
+    paginationClicked();
 });
